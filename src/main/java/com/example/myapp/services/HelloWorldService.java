4a133cbf434a4cb38a4562b62c35360b4e80f6c8
Huskyjjy <jin.jingy@husky.neu.edu>
2020-01-28 16:50:35 -0500

    Created user admin template

---
+++ src/main/java/com/example/myapp/services/HelloWorldService.java
@@ -0,0 +1,10 @@
+package com.example.myapp.services;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloWorldService {
+   @GetMapping("/hello/{name}")
+   public String sayHello(@PathVariable("name") String s1) {
+       return "Hello World" + s1;
+}}
