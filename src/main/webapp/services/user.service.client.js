0ef8080f487cd6c8d90d0ea2c19ab4b802f1d6d2
Huskyjjy <jin.jingy@husky.neu.edu>
2020-01-28 23:59:17 -0500

    half2

--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -4,7 +4,7 @@ function AdminUserServiceClient() {
     this.findUserById = findUserById;
     this.deleteUser = deleteUser;
     this.updateUser = updateUser;
-    this.url = 'https://wbdv-generic-server.herokuapp.com/api/jannunzi/users';
+    this.url = 'https://wbdv-generic-server.herokuapp.com/api/001051733/users';
     var self = this;
     function createUser(user) {
         return fetch(self.url,{
